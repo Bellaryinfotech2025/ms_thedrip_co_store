1be037dcde15b6a82221227f7ed2dcb39eec5855
Bellaryinfotech <bellaryinfotechsolutions@gmail.com>
2025-12-04 22:24:31 +0530

    commit on the port numbe r

--- src/main/java/com/thedripcostore/demo/components/CorsConfig.java
+++ src/main/java/com/thedripcostore/demo/components/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")  // Your React app
+                .allowedOrigins("http://localhost:5178")  // Your React app
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)   
